910ae12cf91e04f33bccdb95024575fb758825a1
adnanali000 <adnan.museel123@gmail.com>
2022-05-12 23:23:36 +0500

    link updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,12 +20,12 @@ const Footer = () => {
       {/* <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@accessories-hub</a> */}
     {/* </p> */}
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a className="text-gray-500" href='' target="_blank">
+      <a className="text-gray-500" href='https://www.facebook.com/thrivestudioo/' target="_blank">
         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
         </svg>
       </a>
-      <a className="ml-3 text-gray-500" href="" target="_blank">
+      <a className="ml-3 text-gray-500" href="https://instagram.com/thrive_studio_?igshid=YmMyMTA2M2Y=" target="_blank">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
           <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
           <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
